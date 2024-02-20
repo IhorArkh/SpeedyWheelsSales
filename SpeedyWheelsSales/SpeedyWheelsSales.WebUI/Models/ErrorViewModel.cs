88266b47af881c77bdde45f408f4efb16c2cc1c5
Ihor Arkhypenko <wasbornindecember@gmail.com>
2024-02-20 20:23:45 +0200

    Solution setup created

---
+++ SpeedyWheelsSales/SpeedyWheelsSales.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SpeedyWheelsSales.WebUI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
