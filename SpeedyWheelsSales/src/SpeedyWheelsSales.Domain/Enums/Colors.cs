2a3af87d4ac54c6348b378c12aa33de9ab4eb785
Ihor Arkhypenko <wasbornindecember@gmail.com>
2024-02-22 09:23:25 +0200

    Basic entities and enums created

---
+++ SpeedyWheelsSales/src/SpeedyWheelsSales.Domain/Enums/Colors.cs
@@ -0,0 +1,18 @@
+﻿namespace Domain.Enums;
+
+public enum Colors
+{
+    Black,
+    White,
+    Silver,
+    Grey, 
+    Blue,
+    Green,
+    Red,
+    Yellow,
+    Orange,
+    Brown,
+    Beige,
+    Pink,
+    Purple
+}
