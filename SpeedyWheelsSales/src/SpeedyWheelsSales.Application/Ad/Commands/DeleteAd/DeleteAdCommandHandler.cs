f2f12ae0f6ba44daa8f00229eeef5e07763bf6b3
Ihor Arkhypenko <wasbornindecember@gmail.com>
2024-03-04 08:26:11 +0200

    Users have ability to delete only their own ads now.

--- SpeedyWheelsSales/src/SpeedyWheelsSales.Application/Ad/Commands/DeleteAd/DeleteAdCommandHandler.cs
+++ SpeedyWheelsSales/src/SpeedyWheelsSales.Application/Ad/Commands/DeleteAd/DeleteAdCommandHandler.cs
@@ -1,5 +1,8 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
 using SpeedyWheelsSales.Application.Core;
+using SpeedyWheelsSales.Application.Interfaces;
 using SpeedyWheelsSales.Infrastructure.Data;
 
 namespace SpeedyWheelsSales.Application.Ad.Commands.DeleteAd;
@@ -7,18 +10,25 @@ namespace SpeedyWheelsSales.Application.Ad.Commands.DeleteAd;
 public class DeleteAdCommandHandler : IRequestHandler<DeleteAdCommand, Result<Unit>>
 {
     private readonly DataContext _context;
+    private readonly IUserAccessor _userAccessor;
 
-    public DeleteAdCommandHandler(DataContext context)
+    public DeleteAdCommandHandler(DataContext context, IUserAccessor userAccessor)
     {
         _context = context;
+        _userAccessor = userAccessor;
     }
 
     public async Task<Result<Unit>> Handle(DeleteAdCommand request, CancellationToken cancellationToken)
     {
-        var ad = await _context.Ads.FindAsync(request.Id);
-
+        var ad = await _context.Ads
+            .Include(x => x.AppUser)
+            .FirstOrDefaultAsync(x => x.Id == request.Id);
+        
         if (ad is null)
             return null;
+
+        if (ad.AppUser.UserName != _userAccessor.GetUsername())
+            return Result<Unit>.Failure("Users can delete only their own ads.");
 
         _context.Remove(ad);
         var result = await _context.SaveChangesAsync() > 0;
