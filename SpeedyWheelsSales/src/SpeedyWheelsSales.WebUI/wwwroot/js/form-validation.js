2901fe50b6780b7dc358e055efb0ed19eef042e1
Ihor Akhypenko <wasbornindecember@gmail.com>
2024-04-14 16:07:07 +0300

    User can't send update profile/ad form without changes now.

---
+++ SpeedyWheelsSales/src/SpeedyWheelsSales.WebUI/wwwroot/js/form-validation.js
@@ -0,0 +1,17 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var forms = document.getElementsByTagName("form");
+
+    for (var i = 0; i < forms.length; i++) {
+        forms[i].addEventListener("submit", function (event) {
+            var inputs = this.querySelectorAll("input, textarea");
+            var hasChanged = Array.from(inputs).some(function (input) {
+                return input.value !== input.defaultValue;
+            });
+
+            if (!hasChanged) {
+                alert("Please make at least one change before submitting the form.");
+                event.preventDefault();
+            }
+        });
+    }
+});
