c967dbece0ee4ecba6231156234bbee2a7eccc37
Ihor Akhypenko <wasbornindecember@gmail.com>
2024-04-20 21:27:12 +0300

    Resource files for en and uk localization ready.

--- SpeedyWheelsSales/src/SpeedyWheelsSales.WebUI/Controllers/HomeController.cs
+++ SpeedyWheelsSales/src/SpeedyWheelsSales.WebUI/Controllers/HomeController.cs
@@ -7,13 +7,6 @@ namespace SpeedyWheelsSales.WebUI.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
     public IActionResult Index()
     {
         return RedirectToAction("ListAds", "Ad");
